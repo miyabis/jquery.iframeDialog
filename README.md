76cf47f1ee2b34acc5c80b159b7947db402ea39c
miyabis <miyabis@me.com>
2012-04-25 16:05:55 +0900

    New Add

---
+++ README.md
@@ -0,0 +1,10 @@
+jQuery UI Dialog iframe wrapper Plugin
+======================
+
+
+License
+----------
+Copyright &copy; 2012 MiYABiS
+Distributed under the [MIT License][mit].
+ 
+[MIT]: http://www.opensource.org/licenses/mit-license.php
---
+++ jquery.iframeDialog.js
@@ -0,0 +1,36 @@
+/*
+ * jQuery UI iframeDialog Plugin v1.0
+ * https://github.com/miyabis/jquery.iframeDialog
+ *
+ * Copyright 2012, MiYABiS
+ * Distributed under the [MIT License][mit].
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+(function ($) {
+    $.fn.iframeDialog = function (options) {
+        this.each(function () {
+            $(this).unbind('click').click(function (e) {
+                e.preventDefault();
+                var $div = $("<div/>");
+                var $iframe = $('<iframe class="iframeDialog" name="iframeDialog{rnd}" frameborder="0" width="100%" height="100%" marginwidth="0" hspace="0" vspace="0" scrolling="no" allowtransparency="true" />');
+                if (!options.title) {
+                    options.title = $(this).attr('title');
+                }
+                if (!options.url) {
+                    options.url = $(this).attr('href');
+                }
+                if (!options.close) {
+                    options.close = function () {
+                        $(this).remove();
+                    };
+                }
+                if (options.scrolling) {
+                    $iframe.attr("scrolling", options.scrolling);
+                }
+                $iframe.attr("src", options.url);
+                $div.append($iframe).dialog(options);
+            });
+        });
+        return this;
+    };
+})(jQuery);
